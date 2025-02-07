f9721a7121fd92d26fd547c53272069cd5dbe759
Abissalaurent <laurentabissa@gmail.com>
2025-02-07 18:32:22 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
+const toggleButton = document.getElementById('toggleButton');
+    const hiddenText = document.getElementById('hiddenText');
 
+    toggleButton.addEventListener('click', () => {
+      if (hiddenText.classList.contains('hidden')) {
+        hiddenText.classList.remove('hidden');
+        toggleButton.textContent = 'Masquer';
+      } else {
+        hiddenText.classList.add('hidden');
+        toggleButton.textContent = 'Présentation';
+      }
+    });
